e2064055c35134915d57d5dd8a42a63a4a93d80a
85785-EGM <2307104918@qq.com>
2023-02-02 08:52:12 +0800

    add not found page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,2 +1,17 @@
-<template>404</template>
+<template>
+  <div class="not-found">404</div>
+</template>
+
 <script setup></script>
+
+<style scoped>
+.not-found {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 60px;
+  letter-spacing: 10px;
+  font-weight: bold;
+}
+</style>
